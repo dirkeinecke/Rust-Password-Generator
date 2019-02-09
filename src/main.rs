54c306ad343b689296cb3eb6d52393c4a0edabad
Dirk Einecke <dirk.einecke@gmail.com>
2019-02-09 16:46:25 +0100

    Initialization of arrays extended

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use rand::prelude::*;
 
 fn main() {
 
-    let lowercase_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    let uppercase_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let symbols = ["!", "§", "$", "%", "&", "?", "@", "#",];
+    let lowercase_letters: [&str; 26] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let uppercase_letters: [&str; 26] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let numbers: [&str; 10] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let symbols: [&str; 8] = ["!", "§", "$", "%", "&", "?", "@", "#",];
 
     let mut rng = thread_rng();
 
