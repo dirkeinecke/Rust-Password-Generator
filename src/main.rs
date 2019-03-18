c85b9d28d6db60990212f23bbc3ab7606c9b1bf2
Dirk Einecke <dirkeinecke@users.noreply.github.com>
2019-03-18 07:04:50 +0100

    fn create_password added

--- src/main.rs
+++ src/main.rs
@@ -11,41 +11,7 @@ struct Config {
     use_symbols: bool,
 }
 
-fn main() {
-
-    let args: Vec<OsString> = env::args_os().collect();
-
-    let mut config = Config {
-        password_length: 10,
-        use_uppercase_letters: args.contains(&OsString::from("mixedcase")),
-        use_numbers: args.contains(&OsString::from("numbers")),
-        use_symbols: args.contains(&OsString::from("symbols")),
-    };
-
-    for a in &args {
-        let a = a.to_str().unwrap_or("");
-
-        if a.contains(&String::from("length=")) {
-            let length: Vec<&str> = a.split('=').collect();
-
-            match length[1].parse::<usize>() {
-                Ok(n) => {
-                    config.password_length = n
-                },
-                Err(error) => {
-                    // kind is not public, so we've to use the description
-                    match error.to_string().as_ref() {
-                        "cannot parse integer from empty string" => eprintln!("Pleace enter a password length."),
-                        "invalid digit found in string" => eprintln!("Pleace enter a valid password length."),
-                        _ => eprintln!("There is something wrong with your password length."),
-                    };
-
-                    eprintln!("Password length is set to default value ({}).", config.password_length);
-                },
-            };
-        }
-    }
-
+fn create_password(config: Config) -> String {
     let lowercase_letters: [&str; 26] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     let uppercase_letters: [&str; 26] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     let numbers: [&str; 10] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -90,6 +56,44 @@ fn main() {
         password.push_str(random_character);
     }
 
-    println!("{}", password);
+    password
+}
+
+fn main() {
+
+    let args: Vec<OsString> = env::args_os().collect();
+
+    let mut config = Config {
+        password_length: 10,
+        use_uppercase_letters: args.contains(&OsString::from("mixedcase")),
+        use_numbers: args.contains(&OsString::from("numbers")),
+        use_symbols: args.contains(&OsString::from("symbols")),
+    };
+
+    for a in &args {
+        let a = a.to_str().unwrap_or("");
+
+        if a.contains(&String::from("length=")) {
+            let length: Vec<&str> = a.split('=').collect();
+
+            match length[1].parse::<usize>() {
+                Ok(n) => {
+                    config.password_length = n
+                },
+                Err(error) => {
+                    // kind is not public, so we've to use the description
+                    match error.to_string().as_ref() {
+                        "cannot parse integer from empty string" => eprintln!("Pleace enter a password length."),
+                        "invalid digit found in string" => eprintln!("Pleace enter a valid password length."),
+                        _ => eprintln!("There is something wrong with your password length."),
+                    };
+
+                    eprintln!("Password length is set to default value ({}).", config.password_length);
+                },
+            };
+        }
+    }
+
+    println!("{}", create_password(config));
 
 }
